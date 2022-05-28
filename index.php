f0ebf1813afdce3173093c8e083506e121ca4f06
khizerdev <46632721+khizerdev@users.noreply.github.com>
2022-05-28 18:55:41 +0500

    Single Product Refactored

--- index.php
+++ index.php
@@ -48,67 +48,9 @@
 
               <?php while($row = $products->fetch_assoc()) { ?>
               <div class="col-xl-3 col-lg-6 col-md-4 col-sm-12 col-12">
-                  <div class="wpo-category-single">
-                      <div class="wpo-category-img">
-                          <img src="<?php echo $row['image']; ?>" alt="">
-                          <ul class="wpo-category-link">
-                              <li><a href="wishlist.html" data-bs-toggle="tooltip"
-                                  data-bs-html="true" title="Add To Wishlist"><i class="fa fa-heart"
-                                          aria-hidden="true"></i></a></li>
-                              <li><a href="cart.html" data-bs-toggle="tooltip"
-                                      data-bs-html="true" title="Add To Cart"><i
-                                          class="fi flaticon-shopping-cart"
-                                          aria-hidden="true"></i></a>
-                              </li>
-                              <li data-bs-toggle="modal" data-bs-target="#popup-quickview">
-                                  <button data-bs-toggle="tooltip" data-bs-html="true"
-                                      title="Quick View"><i class="fi ti-eye"></i></button>
-                              </li>
-                          </ul>
-  
-                      </div>
-                      <div class="wpo-category-text">
-                          <div class="category-reting">
-                              <i class="fa fa-star" aria-hidden="true"></i>
-                              <i class="fa fa-star" aria-hidden="true"></i>
-                              <i class="fa fa-star" aria-hidden="true"></i>
-                              <i class="fa fa-star" aria-hidden="true"></i>
-                              <i class="fa fa-star" aria-hidden="true"></i>
-                          </div>
-                          <h2><a href="product-single.html"><?php echo $row['name']; ?></a></h2>
-                          <div class="product-price">
-                              <ul>
-                                
-                                <?php if($row['discount_price'] != null) {  ?>
 
-                                <li>  
-                                  <?php  echo '$'.$row['discount_price']; ?>
-                                </li>
+                <?php include('components/product.php') ?>
 
-                                <li> 
-                                  <?php echo '$'.$row['price']; ?>
-                                </li>
-
-                                <?php } else { ?>
-
-                                <li>  
-                                 
-                                </li>
-
-                                <li> 
-                                  <?php echo '$'.$row['price']; ?>
-                                </li>
-
-                                <?php }  ?>
-
-                                 
-                                  
-                             
-                                
-                              </ul>
-                          </div>
-                      </div>
-                  </div>
               </div>
               <?php } ?>
              
