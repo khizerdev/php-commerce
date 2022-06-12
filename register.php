5f57c076aa6affbe0f21232df6b8ec9505c86846
khizerdev <46632721+khizerdev@users.noreply.github.com>
2022-06-12 12:21:50 +0500

    Registration Validation Fixed

--- register.php
+++ register.php
@@ -4,7 +4,6 @@ include __DIR__ . "/server/connection.php";
 
 include __DIR__ . "/server/front/register.php";
 
-
 ?>
 
 
--- server/front/register.php
+++ server/front/register.php
@@ -2,6 +2,8 @@
 
 include __DIR__ . "/../connection.php";
 
+session_start();
+
 if(!empty($_POST)) {
 
     $errors = [];
@@ -9,8 +11,7 @@ if(!empty($_POST)) {
     $name = $_POST['name'];
 
     if (empty($name)) {
-
-        session_start();
+  
         $_SESSION['toastr'] = array(
             'type'      => 'error', // or 'success' or 'info' or 'warning'
             'message' => 'Name cannot be empty',
@@ -18,55 +19,118 @@ if(!empty($_POST)) {
         );
 
     } else {
+       
         $email = $_POST['email'];
         $password = $_POST['password'];
         $confirm_password = $_POST['confirm_password'];
+        $validation_fail = false;
     
+
         if($password != $confirm_password) {
-            header('location: register.php');
+         
+      
+            $_SESSION['toastr'] = array(
+                'type'      => 'error', // or 'success' or 'info' or 'warning'
+                'message' => 'Password does not match',
+                'title' =>'Error',
+            );
+
+            $validation_fail = true;
+
+ 
         }
     
         if(strlen($password) < 1) {
-            header('location: register.php');
+
+   
+            $_SESSION['toastr'] = array(
+                'type'      => 'error', // or 'success' or 'info' or 'warning'
+                'message' => 'Password length should be greater than 7',
+                'title' =>'Error',
+            );
+
+            $validation_fail = true;
+         
+        }
+
+        if(!$validation_fail) {
+
+            $check_user = $conn->prepare("SELECT count(*) FROM users WHERE email=?");
+    
+            $check_user->bind_param('s' , $email);
+        
+            if(!$check_user){
+                echo "Prepare failed: (". $conn->errno.") ".$conn->error."<br>";
+            }
+        
+            $check_user->execute();
+            $num_rows = 0;
+            $check_user->bind_result($num_rows);
+            $check_user->store_result();
+            $check_user->fetch();
+        
+         
+            if($num_rows != 0) {
+           
+                $_SESSION['toastr'] = array(
+                    'type'      => 'error', // or 'success' or 'info' or 'warning'
+                    'message' => 'User name already registered',
+                    'title' =>'Error',
+                );
+    
+       
+            } else {
+
+                $sql = "INSERT INTO users(name,email,password) VALUES(?,?,?)";
+        
+                $stmt = $conn->prepare($sql);
+            
+            
+                $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+                $stmt->bind_param('sss' , $name,$email,$password);
+            
+                if(!$stmt){
+                    echo "Prepare failed: (". $conn->errno.") ".$conn->error."<br>";
+                 }
+                 
+                if($stmt->execute()) {
+        
+                    $_SESSION['toastr'] = array(
+                        'type'      => 'success', // or 'success' or 'info' or 'warning'
+                        'message' => 'Registration Successfull',
+                        'title' =>'OK',
+                    );
+        
+                    // $_SESSION['name'] = $name;
+                    // $_SESSION['email'] = $email;
+                    // $_SESSION['login'] = true;
+        
+                    // header('location : account.php');
+        
+        
+                } 
+                else {
+                    echo "Failed";
+                //     session_start();
+        
+                //     $_SESSION['toastr'] = array(
+                //         'type'      => 'error', // or 'success' or 'info' or 'warning'
+                //         'message' => 'Something went wrong',
+                //         'title' =>'Error',
+                //     );
+        
+                //     header('location : register.php');
+                }
+            }
+
+
         }
     
         
-        // $check_user = $conn->prepare("SELECT count(*) from users where email=?");
-    
-        // $check_user->bind_param('s' , $email);
-    
-        // if(!$check_user){
-        //     echo "Prepare failed: (". $conn->errno.") ".$conn->error."<br>";
-        // }
-    
-        // $check_user->execute();
-        // $check_user->bind_result($num_rows);
-        // $check_user->fetch();
-    
-        // if($num_rows != 0) {
-        //     header('location: register.php');
-        // }
+       
     
     
-        $sql = "INSERT INTO users(name,email,password) VALUES(?,?,?)";
-    
-        $stmt = $conn->prepare($sql);
-    
-    
-        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        $stmt->bind_param('sss' , $name,$email,$password);
-    
-        if(!$stmt){
-            echo "Prepare failed: (". $conn->errno.") ".$conn->error."<br>";
-         }
-        $stmt->execute();
-
-        session_start();
-        $_SESSION['toastr'] = array(
-            'type'      => 'success', // or 'success' or 'info' or 'warning'
-            'message' => 'Registration Successfull',
-            'title' =>'OK',
-        );
+     
     }
 
 
